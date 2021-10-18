f33b03c064c80365d71c8f7125224fd80648fb4b
alicandari <alicandarii@icloud.com>
2021-10-19 00:02:06 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Bilmem ne kredisi","Veresiye Kredisi"]
+console.log("<ul>")
+    for(let i = 0;i<konutKredileri.length;i++){
+        console.log("<li>" + konutKredileri.length[i]+"</li>")
+    }
+console.log("</ul>")
+
